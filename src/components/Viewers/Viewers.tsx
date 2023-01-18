98676192698e8e68732e8532b39671900f88beed
PKG-7 <pavel.k.group@gmail.com>
2023-01-19 00:42:33 +0300

    фильмы готовы

--- src/components/Viewers/Viewers.tsx
+++ src/components/Viewers/Viewers.tsx
@@ -1,58 +1,51 @@
-import styled from 'styled-components'
-
+import Image from "next/image";
+import style from "./Viewers.module.scss";
 
 function Viewers() {
-
-
   return (
-    <Container>
-        <Wrap>
-            <img src="/images/viewers-disney.png" alt="" />
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-pixar.png" alt="" />
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-marvel.png" alt="" />
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-starwars.png" alt="" />
-        </Wrap>
-        <Wrap>
-            <img src="/images/viewers-national.png" alt="" />
-        </Wrap>
-    </Container>
-  )
+    <div className={style.container}>
+      <div className={style.wrap}>
+        <Image
+          width={300}
+          height={300}
+          src="/images/viewers-disney.png"
+          alt=""
+        />
+      </div>
+      <div className={style.wrap}>
+        <Image
+          width={300}
+          height={300}
+          src="/images/viewers-pixar.png"
+          alt=""
+        />
+      </div>
+      <div className={style.wrap}>
+        <Image
+          width={300}
+          height={300}
+          src="/images/viewers-marvel.png"
+          alt=""
+        />
+      </div>
+      <div className={style.wrap}>
+        <Image
+          width={300}
+          height={300}
+          src="/images/viewers-starwars.png"
+          alt=""
+        />
+      </div>
+      <div className={style.wrap}>
+        <Image
+          width={300}
+          height={300}
+          src="/images/viewers-national.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
 }
 
-export default Viewers
-
-const Container = styled.div`
-    display: grid;
-    grid-gap: 25px;
-    margin-top: 30px;
-    padding: 30px 0px 26px;
-    grid-template-columns: repeat(5, minmax(0, 1fr));
-`
-
-const Wrap = styled.div`
-    border-radius: 10px;
-    cursor: pointer;
-    transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94) 0s;
-    border: 3px solid rgba(249, 249, 249, 0.1);
-    box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-
-    img {
-        width: 100%;
-        height: 100%;
-        object-fit: cover;
-    }
-
-    &:hover {
-        transform: scale(1.05);
-        box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
-        rgb(0 0 0 / 73%) 0px 16px 10px -10px;
-        border-color: rgba(249, 249, 249, 0.8);
-    }
-`
+export default Viewers;
